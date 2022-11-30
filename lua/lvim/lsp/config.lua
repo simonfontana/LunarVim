60e617f84cba3222a02c004884fd5686bcb95a22
Simon Fontana Oscarsson <simon.fontana.oscarsson@ericsson.com>
2022-11-30 21:43:55 +0100

    Remove python linters to fix install issue
    Setting these up in config fails the install because it then requires
    null-ls which is not installed at the time.
    
    These settings have been moved to my dotfiles repo instead since they
    are environment specific.

--- lua/lvim/lsp/config.lua
+++ lua/lvim/lsp/config.lua
@@ -44,21 +44,6 @@ local skipped_filetypes = { "markdown", "rst", "plaintext", "toml", "proto" }
 
 local join_paths = require("lvim.utils").join_paths
 
--- Python formatters
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  { command = "isort", filetypes = { "python" } },
-  { command = "black", filetypes = { "python" } },
-}
-
--- Python linters
-local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {
-  { command = "flake8", filetypes = { "python" } },
-  { command = "mypy", filetypes = { "python" } },
-  { command = "pylint", filetypes = { "python" } },
-}
-
 return {
   templates_dir = join_paths(get_runtime_dir(), "site", "after", "ftplugin"),
   diagnostics = {
